c198b7b749c94171c5a8b437b1f40804cfb9ea50
incarpub <incarpub@gmail.com>
2026-04-21 20:07:42 +0900

    fix next config and force redeploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'forma-store-api.railway.app'],
+    domains: [
+      'localhost',
+      'forma-store-api.onrender.com',
+      'res.cloudinary.com',
+    ],
   },
   env: {
-    API_URL: process.env.API_URL || 'http://localhost:4000',
+    API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
   },
 }
 
 module.exports = nextConfig
+
+
