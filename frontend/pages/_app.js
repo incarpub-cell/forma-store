fb81ba542a27e9fa1da20ee79a20267677677a5c
incarpub <incarpub@gmail.com>
2026-05-02 21:42:55 +0900

    fix cursor on all pages

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -14,5 +14,43 @@ export default function App({ Component, pageProps }) {
     }
   }, [isAdmin])
 
-  return <Component {...pageProps} />
+  useEffect(() => {
+    if (isAdmin) return
+
+    const cur = document.getElementById('cursor')
+    const ring = document.getElementById('ring')
+    if (!cur || !ring) return
+
+    let mx=0, my=0, rx=0, ry=0, raf
+    const onMove = e => {
+      mx = e.clientX; my = e.clientY
+      cur.style.left = mx + 'px'
+      cur.style.top  = my + 'px'
+    }
+    const animate = () => {
+      rx += (mx - rx) * 0.1
+      ry += (my - ry) * 0.1
+      ring.style.left = rx + 'px'
+      ring.style.top  = ry + 'px'
+      raf = requestAnimationFrame(animate)
+    }
+    animate()
+    window.addEventListener('mousemove', onMove)
+    return () => {
+      window.removeEventListener('mousemove', onMove)
+      cancelAnimationFrame(raf)
+    }
+  }, [router.pathname, isAdmin])
+
+  return (
+    <>
+      {!isAdmin && (
+        <>
+          <div id="cursor" className="cursor" />
+          <div id="ring" className="cursor-ring" />
+        </>
+      )}
+      <Component {...pageProps} />
+    </>
+  )
 }
